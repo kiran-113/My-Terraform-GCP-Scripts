ce7942d20b064b5bbf946171afc1ae3e8b4ac539
Kirankumar <82759393+kiran-113@users.noreply.github.com>
2022-06-20 15:26:34 +0530

    Update install-www.sh

--- createing vm with metadata/scripts/install-www.sh
+++ createing vm with metadata/scripts/install-www.sh
@@ -4,4 +4,41 @@ sudo apt-get install apache2 -y
 sudo a2ensite default-ssl
 sudo a2enmod ssl
 sudo service apache2 restart
-echo '<!doctype html><html><body><h1>GCP-With-Terraform</h1></body></html>' | sudo tee /var/www/html/index.html
+echo 
+  '<!DOCTYPE html>
+<html>
+        <head>
+<style>
+.a{
+background-color: #2471A3;
+color: white;
+padding: 12px 20px;
+border: none;
+border-radius: 4px;
+cursor: pointer;
+float: center;
+}
+.bg {
+/* The image used */
+background-image: url("https://bit.ly/2OEVTYp");
+/* Full height */
+height: 100%;
+/* Center and scale the image nicely */
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;
+}
+.label {
+color: white;
+padding: 8px;
+font-family: Arial; }
+}
+</style>
+</head>
+<body class="bg" style="padding: 210px 0; background-color: #dbfcf9;">
+<center>
+<h3><font size="24"> <font color="white">LevelUp</font></h3>
+<h2><font size="5"> <font color="white">This infra is created with Terraform - ENV0  </font></h2>
+</center>
+</body>
+</html>' | sudo tee /var/www/html/index.html
